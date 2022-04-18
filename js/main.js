b6d1a0f9aa2945603cc67c9cdead4d69cd9759b7
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-04-18 21:25:19 +0500

    Add new change

--- js/main.js
+++ js/main.js
@@ -2,5 +2,9 @@ let elHeader = document.querySelector(".header__list");
 let elBtn = document.querySelector(".header__menu");
 
 elBtn.addEventListener("click", function() {
-    elHeader.classList.toggle("header__list-open")
+    if (!elHeader.matches(".header__list-open")) {
+        elHeader.classList.add("header__list-open");
+    } else {
+        elHeader.classList.remove("header__list-open");
+    }
 })
