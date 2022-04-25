19e87f1a8bb77efcedbac1fec9eb973d5787a47b
Xoliqulov-Nodirbek <xoliqulov.nodirbek2003@gmail.com>
2022-04-25 16:03:46 +0500

    Added footer sklet

--- js/main.js
+++ js/main.js
@@ -10,3 +10,24 @@ elBtn.addEventListener("click", function() {
         elHeader.classList.remove("header__list-open");
     }
 })
+
+let testBtn1 = document.querySelector(".test__buttons-two");
+let testWrapper1 = document.querySelector(".test__content1");
+let testBtn = document.querySelector(".test__buttons-one");
+let testWrapper = document.querySelector(".test__content");
+
+testBtn1.addEventListener("click", function() {
+    if (!testWrapper1.matches(".test__content1--open" )) {
+        testWrapper1.classList.add("test__content1--open");
+    } else {
+        testWrapper1.classList.remove("test__content1--open");
+    }
+});
+
+testBtn.addEventListener("click", function() {
+    if (!testWrapper.matches(".test__content--open" )) {
+        testWrapper.classList.add("test__content--open");
+    } else {
+        testWrapper.classList.remove("test__content--open");
+    }
+});
